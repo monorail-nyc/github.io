a40889e46e5d3690127b462ea1ac1ad4c3ad6729
Zachary Mayberry <zachmayberry@gmail.com>
2015-07-06 18:00:43 -0500

    Adding a new slot for Fresco

--- gruntfile.js
+++ gruntfile.js
@@ -39,5 +39,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default',['concat','uglify']);
+	grunt.registerTask('default',['concat','uglify','sass']);
 }
